64c218b355216fc74de28b5e1bb917987025fcf4
Anak Agung Gde Bagus Janapriya <bgsjanapriya@gmail.com>
2023-10-01 15:51:20 +0800

    initial commit

---
+++ src/components/Practice.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+var provinceId = "Bali"
+
+export default function Practice() {
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      {/* <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
+}
