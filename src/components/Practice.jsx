51dd9a0106708fa120e74cef981b7bf6c9609668
Anak Agung Gde Bagus Janapriya <bgsjanapriya@gmail.com>
2023-10-02 12:23:01 +0800

    connect API

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,17 +1,30 @@
-import React from "react";
-
-var provinceId = "Bali"
+import React, { useState, useEffect } from "react";
+import { api } from "../api/api";
 
 export default function Practice() {
+  const [province, setProvince] = useState([]);
+
+  useEffect(()=>{
+    async function getProvince(){
+      try{
+        const response = await api.get('/location/province');
+        const provinceData = response.data.data
+        setProvince(provinceData)
+      }catch(error){
+        console.log(error.response.data.message)
+      }
+    };
+    getProvince();
+  }, [])
 
   return (
     <div>
       <h1>Weather App</h1>
-      {/* <ul>
-        {data.launchesPast.map((launch) => (
-          <li key={launch.id}>{launch.mission_name}</li>
+      <ul>
+        {province.map((data) => (
+          <li key={data.area_id}>{data.domain}</li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
